b4dda4779874ee6f5cda5454dd9808aa8dc417d8
Javae Vittoni <javaevittoni@gmail.com>
2025-03-25 18:42:34 -0700

    complete delete user listing functionality

--- realestate_marketplace/client/src/pages/Profile.jsx
+++ realestate_marketplace/client/src/pages/Profile.jsx
@@ -17,6 +17,7 @@ export default function Profile() {
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [showListingsError, setShowListingsError] = useState(false);
   const [userListings, setUserListings] = useState([]);
+  const [deleteListingsError, setDeleteListingsError] = useState(false);
   const dispatch = useDispatch();
 
   // Firebase Storage Rules
@@ -130,6 +131,26 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      setDeleteListingsError(false);
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        // console.log(data.message);
+        setDeleteListingsError(true);
+        return;
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      // console.log(error.message);
+      setDeleteListingsError(true);
+      return
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -182,8 +203,8 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className='flex flex-col items-center'>
-                <button className='text-green-700 uppercase'>Edit</button>
-                <button className='text-red-600 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase cursor-pointer'>Edit</button>
+                <button onClick={()=>handleListingDelete(listing._id)} className='text-red-600 uppercase cursor-pointer'>Delete</button>
               </div>
             </div>
           ))}
