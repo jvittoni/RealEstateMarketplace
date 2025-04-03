a1b468ad1b9fe0d4f1851fd5756c1457d5b57c10
Javae Vittoni <javaevittoni@gmail.com>
2025-04-02 19:18:04 -0700

    complete about page

--- realestate_marketplace/client/src/pages/About.jsx
+++ realestate_marketplace/client/src/pages/About.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Real Estate</h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt eaque quia non error ab unde officia, optio ipsa iste sapiente nesciunt voluptates est maiores harum iusto voluptatem dicta labore. Soluta.
+        Sapiente, fuga, quod, asperiores illum accusamus rem praesentium ea dolorem unde harum amet.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt eaque quia non error ab unde officia, optio ipsa iste sapiente nesciunt voluptates est maiores harum iusto voluptatem dicta labore. Soluta.
+        Sapiente, fuga, quod, asperiores illum accusamus rem praesentium ea dolorem unde harum amet.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt eaque quia non error ab unde officia, optio ipsa iste sapiente nesciunt voluptates est maiores harum iusto voluptatem dicta labore. Soluta.
+        Sapiente, fuga, quod, asperiores illum accusamus rem praesentium ea dolorem unde harum amet.
+      </p>
+    </div>
   )
 }
